6a50a696f18616c0a90447542e06a414296506b1
Dan Kim <dankim@Dans-MacBook-Air.local>
2021-12-21 21:54:12 -0800

    finalized for the submission

--- app.js
+++ app.js
@@ -1,96 +1,170 @@
-    // Create Dino Constructor, inherited from animal
+// Create Dino Constructor
 function Dino(species, weight, height, diet, fact) {
-    this.species = species;
-    this.weight = weight;
-    this.height = height;
-    this.diet = diet;
-    this.facts = fact;
-    this.image = "images/" + species.toLowerCase() + ".png";
+  this.species = species;
+  this.weight = weight;
+  this.height = height;
+  this.diet = diet;
+  this.facts = fact;
+  this.image = "images/" + species.toLowerCase() + ".png";
 }
 
-    // Create Dino Object
-
-    // Create Human Object
-    // inherited from dino
-function Human(species, weight, height, diet, fact) {
-    Dino.call(this, species, weight, height, diet, fact);
-}
-// construct inheritance based on prototype
-Human.prototype = Object.create(Dino.prototype);
-Human.prototype.constructor = Human;
-
-    // Create Dino Compare Method 1.
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-Dino.prototype.compareWeight = function(weight) {
-    let fact;
-    if (this.weight > weight) {
-        fact = "I am heavier than you.";
-    }else if (this.weight < weight) {
-        fact = "You are heavier than me.";
-    }else {
-        fact = "Our weights are the same.";
-    }
-    this.facts.push(fact);
-}
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-Dino.prototype.compareHeight = function(height) {
-    let fact;
-    if (this.height > height) {
-        fact = "I am taller than you.";
-    }else if (this.height < height) {
-        fact = "You are taller than me.";
-    }else {
-        fact = "Our heights are the same.";
-    }
-    this.facts.push(fact);
+// Create Dino Compare Method 1.
+// NOTE: Weight in JSON file is in lbs, height in inches.
+Dino.prototype.compareWeight = function (weight) {
+  let fact;
+  if (this.weight > weight) {
+    fact = "I am heavier";
+  } else if (this.weight < weight) {
+    fact = "I am lighter";
+  } else {
+    fact = "Our weights are the same.";
+  }
+  this.facts.push(fact);
 };
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-Dino.prototype.compareDiet = function(diet) {
-    let fact;
-    if (this.diet == diet) {
-        fact = `We are both ${this.diet}.`;
-    } else {
-        fact = `I am ${this.diet}, but you are ${diet}.`;
-    }
-    this.facts.push(fact);
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+Dino.prototype.compareHeight = function (height) {
+  let fact;
+  if (this.height > height) {
+    fact = "I am taller";
+  } else if (this.height < height) {
+    fact = "I am shorter";
+  } else {
+    fact = "Our heights are the same";
+  }
+  this.facts.push(fact);
+};
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+Dino.prototype.compareDiet = function (diet) {
+  let fact;
+  if (this.diet == diet) {
+    fact = `We are both ${diet}`;
+  } else {
+    fact = `I am ${this.diet}`;
+  }
+  this.facts.push(fact);
 };
 
 // creater Human object by inheriting Dino
-function Human(name, weight, height) {
-    Dino.call(this, "human", weight, height, "Omnibore", []);
-    this.name = name; 
+function Human(name, weight, height, diet) {
+  Dino.call(this, "human", weight, height, diet, [`I am a human and ${diet}`]);
+  this.name = name;
 }
 // construct inheritance based on prototype
 Human.prototype = Object.create(Dino.prototype);
 Human.prototype.constructor = Human;
-
-// Use IIFE to get human data from form    
-const getHuman = (() => {
-    let name = document.getElementById("name").value;
-    let heightFeet = parseFloat(document.getElementById("feet").value).toFixed(2);
-    let heightInches = parseFloat(document.getElementById("inches").value).toFixed(2);
-    let weight = document.getElementById("weight").value;
-
-    return new Human(name, weight, heightFeet * 12 + heightInches);
-})();
-// fs.readfile() and make dinos[] and add each dino objects. 
-// to do this 
 
 var dinos = [];
 
 fetch("dino.json")
-    .then(x => x.json())
-    .then(json => dinos = json.Dinos.map(dino => new Dino(dino.species, dino.weight, )))
+  .then((x) => x.json())
+  .then(
+    (json) =>
+      (dinos = json.Dinos.map(
+        (dino) =>
+          new Dino(
+            dino.species,
+            parseInt(dino.weight, 10),
+            parseInt(dino.height, 10),
+            dino.diet,
+            [dino.fact, `I lived in ${dino.where} during ${dino.when}.`]
+          )
+      ))
+  );
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+// Use IIFE to get human data from form
+function getHuman() {
+  return (() => {
+    let name = document.getElementById("name").value;
+    let heightFeet = parseFloat(document.getElementById("feet").value);
+    let heightInches = parseFloat(document.getElementById("inches").value);
+    let height = heightFeet * 12 + heightInches;
+    let weight = document.getElementById("weight").value;
+    let diet = document.getElementById("diet").value.toLowerCase();
 
-    // Remove form from screen
+    return new Human(name, weight, height, diet);
+  })();
+}
 
+/***** DOM manipulation *****/
+
+// Generate Tiles for each Dino in Array
+function generateDinoTiles(species, image, fact) {
+  // Add tiles to DOM
+  /*
+  <div class="grid-item">
+    <h3>species or name</h3>
+    <img>image</img>
+    <p>fact1</p>
+  </div>
+  */
+
+  // <div class="grid-item">
+  let gridTileDiv = document.createElement("div");
+  gridTileDiv.className = "grid-item";
+
+  // <h3>species or name</h3>
+  let speciesH3 = document.createElement("h3");
+  speciesH3.innerText = species;
+  gridTileDiv.appendChild(speciesH3);
+
+  // <img>image</img>
+  let imageImg = document.createElement("img");
+  imageImg.src = image;
+  gridTileDiv.appendChild(imageImg);
+
+  // <p>fact1</p>
+  if (fact) {
+    let factP = document.createElement("p");
+    factP.innerText = fact;
+    gridTileDiv.appendChild(factP);
+  }
+
+  return gridTileDiv;
+}
 
 // On button click, prepare and display infographic
+document.getElementById("btn").addEventListener("click", () => {
+  // compare dino and human
+  const human = getHuman();
+  dinos.forEach((dino) => {
+    dino.compareWeight(human.weight);
+    dino.compareHeight(human.height);
+    dino.compareDiet(human.diet);
+  });
+
+  // Remove form from screen
+  document.getElementById("dino-compare").remove();
+
+  // Generate Tiles for each Dino in Array
+  for (let idx in dinos) {
+    let dino = dinos[idx];
+
+    // random fact in fracts array
+    let randomIdx = Math.floor(Math.random() * 10) % dino.facts.length;
+    let fact = dino.facts[randomIdx];
+    
+    // if bird
+    if (dino.weight < 1) {
+      fact = dino.facts[0];  // "All birds are living dinosaurs."
+    }
+
+    let dinoTile = generateDinoTiles(dino.species, dino.image, fact);
+
+    // display
+    document.getElementById("grid").appendChild(dinoTile);
+
+    /* center must be a human: 3 by 3 grid so... 0 to 8.
+    012
+    345
+    678
+    Thus center idx must be after 3 if we count left-top to right-bottom */
+    if (idx == 3) {
+      let humanTile = generateDinoTiles(human.name, human.image, human.facts[0]);
+      document.getElementById("grid").appendChild(humanTile);
+    }
+  }
+});
